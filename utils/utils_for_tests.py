a8066bc350e5fe2d10acc3e29f7c3cf76e41f7ab
Alex-Zozulia-1997 <oleksandr97zozulia@gmail.com>
2024-05-08 21:25:17 +0200

    nb

--- utils/utils_for_tests.py
+++ utils/utils_for_tests.py
@@ -1,12 +1,14 @@
 import requests
 
+import os
+
 
 def get_token():
+    base_url = os.getenv("TEST_BASE_URL", "http://127.0.0.1:5000")
     response = requests.post(
-        "http://127.0.0.1:5000/login",
+        f"{base_url}/login",
         json={"username": "salex@alex.comii", "password": "12345678a"},
     )
-
     print(response.status_code)
     token = response.json()["access_token"]
     return token
