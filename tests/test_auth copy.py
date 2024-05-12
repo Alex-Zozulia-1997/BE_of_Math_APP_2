e531033d16834ecdca59aa1081ac7226fe284080
Alex-Zozulia-1997 <oleksandr97zozulia@gmail.com>
2024-05-12 19:53:36 +0200

    fucking unittest

---
+++ tests/test_auth copy.py
@@ -0,0 +1,37 @@
+import unittest
+from app import create_app
+from flask import json
+from utils.utils_for_tests import get_token
+
+
+class TestAuthRoutes(unittest.TestCase):
+
+    def setUp(self):
+        # Setup the app with testing configurations
+        self.app = create_app("sqlite:///data.db")
+        self.app.config["TESTING"] = True
+        self.client = self.app.test_client()
+
+    def test_login(self):
+        # Testing login with correct credentials
+        user_data = {"username": "nadia@nekrysa.com", "password": "12345678a"}
+        response = self.client.post("/login", json=user_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_wrong_login_credentials(self):
+        # Testing login with incorrect credentials
+        user_data = {"username": "nadia@nekrysa.com11111", "password": "12345678a"}
+        response = self.client.post("/login", json=user_data)
+        self.assertEqual(response.status_code, 401)
+
+    def test_logout(self):
+        # Testing logout
+        access_token = get_token()
+        headers = {"Authorization": f"Bearer {access_token}"}
+        user_data = {"username": "nadia@nekrysa.com", "password": "12345678a"}
+        response = self.client.post("/logout", json=user_data, headers=headers)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
